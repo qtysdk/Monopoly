4238564f5f66cd3b947445f73716590f66552b58
Ching Yi, Chan <qrtt1@infuseai.io>
2023-09-17 19:22:23 +0800

    Init
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

---
+++ src/main/java/Monopoly.java
@@ -0,0 +1,10 @@
+public class Monopoly {
+
+    private Player[] players;
+    private GameMap gameMap;
+
+    public Monopoly(Player[] players, GameMap gameMap) {
+        this.players = players;
+        this.gameMap = gameMap;
+    }
+}
